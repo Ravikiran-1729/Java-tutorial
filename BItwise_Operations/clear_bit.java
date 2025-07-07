a6b86021deb63cda8620b1004e0f646fe5b14708
Ravikiran Pradip Yavalkar <ravikiranpyawalkar@gmail.com>
2025-07-08 01:56:18 +0530

    Add files via upload

---
+++ BItwise_Operations/clear_bit.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class clear_bit {
+    public static void main(String[] args) {
+        int n = 5;//0101
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a position :- ");
+        int pos = sc.nextInt();
+
+        int bitmask = 1 << pos;
+
+        int newnumber = (~ bitmask) & n;
+
+        System.out.println(newnumber);
+    }
+}
