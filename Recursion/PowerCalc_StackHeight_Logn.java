4c7911676b76582d4f26c2554674e89a08b33792
Ravikiran-1729 <ravikiranpyawalkar@gmail.com>
2025-07-11 22:58:47 +0530

    update&practice

---
+++ Recursion/PowerCalc_StackHeight_Logn.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class PowerCalc_StackHeight_Logn {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int x = sc.nextInt();
+        System.out.print("Enter a power: ");
+        int n = sc.nextInt();
+
+        if (n < 0) {
+            System.out.println("Negative powers not supported in this version.");
+        } else {
+            int result = calculatePower(x, n);
+            System.out.println(x + " to the power " + n + " is " + result);
+        }
+
+        sc.close();
+    }
+
+    public static int calculatePower(int x, int n) {
+        if (x == 0) return 0;
+        if (n == 0) return 1;
+        if (n == 1) return x;
+        if(n % 2 == 0){
+            int halfPower = calculatePower(x, n/2);
+            return halfPower * halfPower;
+        }else{
+            int halfPower = calculatePower(x, n/2);
+            return x * halfPower * halfPower;
+        }
+    }
+}
