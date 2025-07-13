f3fbddf08f97914189773f8655b7e0e5ddc7990d
Ravikiran-1729 <ravikiranpyawalkar@gmail.com>
2025-07-13 17:49:30 +0530

    update

--- Intermidiate_Recursion/first&lastOccaranceString.java
+++ Intermidiate_Recursion/first&lastOccaranceString.java
@@ -7,5 +7,27 @@ public class FirstLastOccurrenceString {
         System.out.print("Enter String :-");
         String str = sc.nextLine();
 
+        findFirstLastOcc(str, -1, -1, str.length()-1);
+
+    }
+
+    public static void findFirstLastOcc(String str, int first, int last, int len){
+        char a = 'a';
+
+        if(len < 0){
+            return;
+        }
+
+        if(str.charAt(len) == a){
+            if(first == -1){
+                first = len;
+                System.out.println(first);
+            }else{
+                last = len;
+            }
+        }
+
+        findFirstLastOcc(str,first,last,len-1);
+        System.out.println(last);
     }
 }
