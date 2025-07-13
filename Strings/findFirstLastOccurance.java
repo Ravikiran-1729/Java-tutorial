983ee36abddaed9645a2702ebf4e7b2439e05fdd
Ravikiran-1729 <ravikiranpyawalkar@gmail.com>
2025-07-13 18:24:28 +0530

    practice

---
+++ Strings/findFirstLastOccurance.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class findFirstLastOccurance {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a String:- ");
+        String str = sc.nextLine();
+
+        System.out.print("Enter a character element :- ");
+        char element = sc.next().charAt(0);
+
+        int first = str.indexOf(element);
+        int last = str.lastIndexOf(element);
+
+        
+        System.out.println("First Occurance of character " + element + " is at index " + first);
+        System.out.println("last Occurance of character " + element + " is at index " + last);
+    }
+}
