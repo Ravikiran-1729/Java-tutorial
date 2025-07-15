89d6ba8603e937a41901ba34f456603d701f4a28
Ravikiran-1729 <ravikiranpyawalkar@gmail.com>
2025-07-16 01:23:21 +0530

    update NQueen file

--- BackTracking/NQueens.java
+++ BackTracking/NQueens.java
@@ -6,19 +6,18 @@ import java.util.Scanner;
 public class NQueens {
 
     public static void saveboard(char board[][], List<List<String>> allBoards){
-        String row = "";
         List<String> newlist = new ArrayList<>();
 
         for(int i = 0; i<board.length; i++){
-            row = "";
+            StringBuilder row = new StringBuilder();
             for(int j = 0; j<board[0].length; j++){
                 if(board[i][j]== 'Q'){
-                    row += "Q  ";
+                    row.append("Q  ");
                 }else{
-                    row += "_  ";
+                    row.append("_  ");
                 }
             }
-            newlist.add(row);
+            newlist.add(row.toString());
         }
         allBoards.add(newlist);
 
@@ -26,67 +25,86 @@ public class NQueens {
     }
 
     public static boolean isSafe(int row, int col, char[][] board){
-        for(int j = 0; j<board.length; j++){
+
+        // Check horizontal row TO THE LEFT
+        for(int j = col; j>=0; j--){
             if(board[row][j]== 'Q'){
                 return false;
             }
         }
+        
+        // // NO NEED TO CHECK THE SAME ROW TO THE RIGHT BECAUSE WE PLACING QUEENS FROM LEFT -> RIGHT
 
-        for(int i = 0; i<board.length; i++){
-            if(board[i][col]== 'Q'){
-                return false;
-            }
-        }
+        // // Check horizontal row
+        // for(int j = 0; j<board.length; j++){
+        //     if(board[row][j]== 'Q'){
+        //         return false;
+        //     }
+        // }
 
+        
+        // // NO NEED TO CHECK BECAUSE WE PLACING QUEENS FROM LEFT -> RIGHT
+
+        // // Check Vertical Column
+        // for(int i = 0; i<board.length; i++){
+        //     if(board[i][col]== 'Q'){
+        //         return false;
+        //     }
+        // }
+
+        // Check Upper left diagonal
         for(int r = row, c = col; r>=0 && c>=0; r--, c--){
             if(board[r][c]=='Q'){
                 return false;
             }
         }
 
-        
+
+        // Check Lower left diagonal
         for(int r = row, c = col; r<board.length && c>=0; r++, c--){
             if(board[r][c]=='Q'){
                 return false;
             }
         }
 
-        for(int r = row, c = col; r>=0 && c<board.length; r--, c++){
-            if(board[r][c]=='Q'){
-                return false;
-            }
-        }
-        for(int r = row, c = col; r<board.length && c<board.length; r++, c++){
-            if(board[r][c]=='Q'){
-                return false;
-            }
-        }
+
+        // // NO NEED TO CHECK BECAUSE WE PLACING QUEENS FROM LEFT -> RIGHT
+
+        // // Check Upper Right
+        // for(int r = row, c = col; r>=0 && c<board.length; r--, c++){
+        //     if(board[r][c]=='Q'){
+        //         return false;
+        //     }
+        // }
+
+        // // Check Lower Right
+        // for(int r = row, c = col; r<board.length && c<board.length; r++, c++){
+        //     if(board[r][c]=='Q'){
+        //         return false;
+        //     }
+        // }
 
         return true;
     }
 
+
+
     public static void helper(List<List<String>> allBoards, char board[][], int col){
         if(col == board.length){
-            // for(int i = 0; i<board.length; i++){
-            //     for(int j = 0; j<board.length; j++){
-            //         System.out.print(board[i][j]+" ");
-            //     }
-            //     System.out.println();
-            // }
-            
-            // System.out.println();
-            // System.out.println();
             saveboard(board, allBoards);
             return;
         }
+
         for(int row = 0; row<board.length; row++){
             if(isSafe(row, col, board)){
                 board[row][col] = 'Q';
                 helper(allBoards, board, col+1);
-                board[row][col] = '.';
+                board[row][col] = '_';
             }
         }
     }
+
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -98,15 +116,25 @@ public class NQueens {
 
         helper(allBoards, board, 0);
 
-        int i = 0;
-        while(allBoards.size()>i){
-            int j = 0;
-            while (allBoards.get(i).size()>j) {
-                System.out.println(allBoards.get(i).get(j));
-                j++;
+        // int i = 0;
+        // while(allBoards.size()>i){
+        //     int j = 0;
+        //     while (allBoards.get(i).size()>j) {
+        //         System.out.println(allBoards.get(i).get(j));
+        //         j++;
+        //     }
+        //     System.out.println();
+        //     i++;
+        // }
+
+
+        int i = 1;
+        for(List<String> newboard : allBoards){
+            System.out.println("Solution #"+i++);
+            for(String row : newboard){
+                System.out.println(row);
             }
             System.out.println();
-            i++;
         }
 
         System.out.println("There are " + allBoards.size() + " valid ways to place " + n + " queens on a " + n + " X " + n + " chessboard.");
