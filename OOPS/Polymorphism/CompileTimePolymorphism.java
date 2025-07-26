8ca62e9909cb08e1cd5a9a53ff42f0349619dd13
Ravikiran-1729 <ravikiranpyawalkar@gmail.com>
2025-07-26 19:30:31 +0530

    update

---
+++ OOPS/Polymorphism/CompileTimePolymorphism.java
@@ -0,0 +1,30 @@
+package OOPS.Polymorphism;
+
+class Student{
+    String name;
+    int age;
+
+    public void getInfo(String name){
+        System.out.println(name);
+    }
+
+    public void getInfo(int age){
+        System.out.println(age);
+    }
+
+    public void getInfo(String name, int age){
+        System.out.println(name+" "+age);
+    }
+
+}
+public class CompileTimePolymorphism {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.name = "Ravikiran";
+        s1.age = 19;
+
+        s1.getInfo(s1.name);
+        s1.getInfo(s1.age);
+        s1.getInfo(s1.name, s1.age);
+    }
+}
