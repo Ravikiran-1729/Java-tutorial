fde253b70dd212932e7ab4863872f29e00db366a
Ravikiran-1729 <ravikiranpyawalkar@gmail.com>
2025-07-26 19:48:15 +0530

    update

--- OOPS/Polymorphism/runTimePolymorphism.java
+++ OOPS/Polymorphism/runTimePolymorphism.java
@@ -1,5 +1,29 @@
-package OOPS.Polymorphism;
+class Shape{
+    public void area(){
+        System.out.println("Display Area of Shape");
+    }
+}
+
+class Triangle extends Shape{
+    public void area(int h, int b){
+        System.out.println("Area : "+1/2*h*b);
+    }
+}
+
+class Circle extends Shape{
+    public void area(int r){
+        System.out.println("Area : "+3.14*r*r);
+    }
+}
 
 public class runTimePolymorphism {
-    
+    public static void main(String[] args) {
+        Triangle t1 = new Triangle();
+        t1.area();
+        t1.area(10, 10);
+
+        Circle c1 = new Circle();
+        c1.area();
+        c1.area(14);
+    }
 }
