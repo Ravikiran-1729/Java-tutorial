61d8a43fff2fa7b49d12e90af98db9ce2aa352d4
Ravikiran-1729 <ravikiranpyawalkar@gmail.com>
2025-07-28 16:04:10 +0530

    linked list declaration

--- LinkedLists/DeclarationLL.java
+++ LinkedLists/DeclarationLL.java
@@ -1,75 +1,20 @@
-// public class DeclarationLL {
-//     Node head;
-
-//     class Node {
-//         String data;
-//         Node next;
-
-//         Node(String data) {
-//             this.data = data;
-//             this.next = null;
-//         }
-//     }
-
-//     public void addfirst(String data) {
-//         Node newNode = new Node(data);
-//         if (head == null) {
-//             head = newNode;
-//             return;
-//         }
-//         newNode.next = head;
-//         head = newNode;
-//     }
-
-//     public void addlast(String data) {
-//         Node newNode = new Node(data);
-//         if (head == null) {
-//             head = newNode;
-//             return;
-//         }
-
-//         Node curNode = head;
-//         while (curNode != null) {
-//             curNode = curNode.next;
-//         }
-//         curNode.next = newNode;
-//     }
-
-//     public void printlist() {
-//         if(head == null){
-//             System.out.println("List is Empty");
-//             return;
-//         }
-//         Node curNode = head;
-//         while (curNode == null) {
-//             System.out.print(curNode.data + " -> ");
-//             curNode = curNode.next;
-//         }
-//     }
-
-//     public static void main(String[] args) {
-//         DeclarationLL list = new DeclarationLL();
-//         list.addfirst("a");
-//         list.addfirst("list");
-//         list.printlist();
-//     }
-// }
 class DeclarationLL {
     Node head;
-    class Node{
+
+    class Node {
         String data;
         Node next;
 
-        Node(String data){
+        Node(String data) {
             this.data = data;
             this.next = null;
         }
     }
 
-    public void addfirst(String data){
+    public void addfirst(String data) {
         Node newNode = new Node(data);
 
-        if(head == null){
+        if (head == null) {
             head = newNode;
             return;
         }
@@ -78,43 +23,88 @@ class DeclarationLL {
         head = newNode;
     }
 
-    public void addlast(String data){
+    public void addlast(String data) {
         Node newNode = new Node(data);
-        if(head == null){
+        if (head == null) {
             head = newNode;
             return;
         }
 
         Node currNode = head;
-        while(currNode != null){
+        while (currNode.next != null) {
             currNode = currNode.next;
         }
         currNode.next = newNode;
     }
 
-    public void printlist(){
-        if(head == null){
+    public void printlist() {
+        if (head == null) {
             System.out.println("List is Empty");
             return;
         }
         Node currNode = head;
-        while (currNode == null){
-            System.out.print(currNode+" -> ");
+        while (currNode != null) {
+            System.out.print(currNode.data + " -> ");
             currNode = currNode.next;
         }
-        System.out.print("NULL");
+        System.out.println("NULL");
+        
     }
 
     public static void main(String[] args) {
         DeclarationLL list = new DeclarationLL();
         list.addfirst("a");
         list.printlist();
+
         list.addfirst("is");
         list.printlist();
         list.addfirst("This");
         list.printlist();
         list.addlast("list");
         list.printlist();
+        // list.deletefirst();
+        // list.printlist();
+        // list.deletelast();
+        // list.printlist();
+        list.deleteelement(4);
+        list.printlist();
     }
-     
+
+    public void deletefirst(){
+        
+        if(head == null){
+            System.err.println("List is Empty");
+            return;
+        }
+        Node currNode = head;
+        head = currNode.next;
+    }
+
+    public void deletelast(){
+        Node currNode = head;
+        while((currNode.next).next != null){
+            currNode = currNode.next;
+        }
+        currNode.next = null;
+    }
+
+    public void deleteelement(int idx){
+        
+        Node currNode = head;
+        if(idx == 0){
+            head = currNode.next;
+            return;
+        }
+
+        for(int i = 1; i<idx; i++){
+            currNode = currNode.next;
+            if(currNode.next == null){
+                System.err.println("idx Out of Bound");
+                return;
+            }
+        }
+
+        currNode.next = (currNode.next).next;
+    }
+
 }
