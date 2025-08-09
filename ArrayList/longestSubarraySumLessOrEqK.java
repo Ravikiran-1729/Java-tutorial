53c181fc51e0fd5f5c2e355610529976b95bc7a7
Ravikiran-1729 <ravikiranpyawalkar@gmail.com>
2025-08-09 18:34:36 +0530

    shortest subarrylist sum >= target

---
+++ ArrayList/longestSubarraySumLessOrEqK.java
@@ -0,0 +1,35 @@
+// Find the length of the longest subarray with sum ≤ k (non-negative integers).
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class longestSubarraySumLessOrEqK {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(1,2,3,4,5));
+
+        int k_sum = 14;
+        ArrayList<Integer> longest = new ArrayList<>();
+
+        subArrayLists(list, 0, k_sum, new ArrayList<>(), 0,  longest);
+        
+        System.out.println("Longest subarray with sum ≤ "+ k_sum + " is :- " + longest);
+        System.out.println("Length of longest subarray with sum = "+ k_sum + " is :- " + longest.size());
+    }
+
+
+
+    public static void subArrayLists(ArrayList<Integer> list, int idx, int sum, ArrayList<Integer> curr_subList, int curr_sum, ArrayList<Integer> longest){
+        if(idx == list.size()){
+            if(curr_sum <= sum && longest.size() <= curr_subList.size()){
+                longest.clear();
+                longest.addAll(curr_subList);
+            }
+            return;
+        }
+        curr_subList.add(list.get(idx));
+        subArrayLists(list, idx+1, sum, curr_subList, curr_sum+list.get(idx), longest);
+
+        curr_subList.remove(curr_subList.size()-1);
+        subArrayLists(list, idx+1, sum,  curr_subList, curr_sum, longest);
+    }
+}
---
+++ ArrayList/shortestSubarraysumGreaterOrEqK.java
@@ -0,0 +1,39 @@
+// Find the length of the longest subarray with sum ≤ k (non-negative integers).
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class shortestSubarraysumGreaterOrEqK {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(1,2,3,4,5));
+
+        int k_sum = 5;
+        ArrayList<Integer> shortest = new ArrayList<>();
+
+        subArrayLists(list, 0, k_sum, new ArrayList<>(), 0,  shortest);
+        
+        System.out.println("Shortest subarray with sum >= "+ k_sum + " is :- " + shortest);
+        System.out.println("Length of shortest subarray with sum >= "+ k_sum + " is :- " + shortest.size());
+    }
+
+
+
+    public static void subArrayLists(ArrayList<Integer> list, int idx, int k_sum, ArrayList<Integer> curr_subList, int curr_sum, ArrayList<Integer> shortest){
+        if(idx == list.size()){
+            if(curr_sum >= k_sum ){
+                if(shortest.size() == 0){
+                    shortest.addAll(curr_subList);
+                }else if(shortest.size() >= curr_subList.size()){
+                    shortest.clear();
+                    shortest.addAll(curr_subList);
+                }
+            }
+            return;
+        }
+        curr_subList.add(list.get(idx));
+        subArrayLists(list, idx+1, k_sum, curr_subList, curr_sum+list.get(idx), shortest);
+
+        curr_subList.remove(curr_subList.size()-1);
+        subArrayLists(list, idx+1, k_sum,  curr_subList, curr_sum, shortest);
+    }
+}
