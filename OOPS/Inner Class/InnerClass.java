f1348b2ae508aee79d5a8c95747ee4cce2461f8f
Ravikiran-1729 <ravikiranpyawalkar@gmail.com>
2025-12-14 20:40:49 +0530

    Defined Inner Class

---
+++ OOPS/Inner Class/InnerClass.java
@@ -0,0 +1,44 @@
+// Outer class
+class A {
+
+    int x;  // Instance variable of outer class
+
+    // -----------------------------------------
+    // Inner class (Non-static nested class)
+    // -----------------------------------------
+    // ✔ Can access all members of outer class
+    // ✔ Requires an object of outer class to create
+    class B {
+
+        int y;  // Instance variable of inner class
+
+        // Method of inner class
+        void showInner(){
+            System.out.println("This is inner class method");
+        }
+    }
+
+    // Method of outer class
+    void showOuter(){
+        System.out.println("This is outer class method");
+    }
+}
+
+public class InnerClass {
+
+    public static void main(String[] args) {
+
+        // Creating object of outer class
+        A a = new A();
+
+        // Creating object of inner class
+        // Syntax: outerObject.new InnerClass()
+        A.B b = a.new B();
+
+        // Calling inner class method
+        b.showInner();
+
+        // Calling outer class method
+        a.showOuter();
+    }
+}
