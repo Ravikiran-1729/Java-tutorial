4a385d14e50b8cc4703a50a4213de353f99d9d9d
Ravikiran-1729 <ravikiranpyawalkar@gmail.com>
2025-12-18 15:54:12 +0530

    Defined Majority Element identifing class in array

--- Arrays/MajorityElement.java
+++ Arrays/MajorityElement.java
@@ -2,25 +2,25 @@ public class MajorityElement {
     private static int majorityElement(int nums[]){
         int candidate = nums[0];
         int count = 1;
-        for(int i = 1; i<nums.length; i++){
+        for(int num : nums){
             if(count == 0){
-                candidate = nums[i];
-                count++;
-                continue;
+                candidate = num;
             }
-            if(nums[i] == candidate){
-                count++;
-            }else{
-                count--;
-            }
+            count += (candidate == num) ? 1 : -1;
         }
-        return candidate;
+        count = 0;
+        for(int num:nums){
+            if(candidate == num){count++;}
+        }
+
+        return (count > nums.length/2) ? candidate : -1;
     }
     public static void main(String[] args) {
         // int arr[] = {2,2,1,1,1,2,2};
         // int arr[] = {2,2,1,3,4,2,2};
         // int arr[] = {2,0,0,0,0,2,2};
-        int arr[] = {3,2,3};
+        // int arr[] = {3,2,3};
+        int arr[] = {3,2,4};
         int majorityEl = majorityElement(arr);
         System.out.println(majorityEl);
     }
